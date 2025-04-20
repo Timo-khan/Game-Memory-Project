8c2ac8de487f01c9cbbea09b57305af67b1bb50f
Timo <timokhan2@gmail.com>
2025-04-20 10:27:18 +0200

    card list images added

---
+++ game.js
@@ -0,0 +1,4 @@
+const cardsList = [
+    'bird2.jpg', 'bird2.jpg', 'cat.jpg', 'cat.jpg', 'parrot.jpg', 'parrot.jpg',
+    'ostrich.avif', 'ostrich.avif', 'squirrel.jpg', 'squirrel.jpg', 'turtle.jpg', 'turtle.jpg'  
+];
