deb8e91e573445137b79364dc25e2a21c19e870a
Timo <timokhan2@gmail.com>
2025-04-20 11:22:51 +0200

    game grid variable created

--- game.js
+++ game.js
@@ -2,5 +2,18 @@ const cardsList = [
     'bird2.jpg', 'bird2.jpg', 'cat.jpg', 'cat.jpg', 'parrot.jpg', 'parrot.jpg',
     'ostrich.avif', 'ostrich.avif', 'squirrel.jpg', 'squirrel.jpg', 'turtle.jpg', 'turtle.jpg'  
 ];
-
 cardsList.sort(() => 0.5 - Math.random());
+
+const grid = document.querySelector(".gameGrid");
+const cardsInGame = 6;
+attempts = 0;
+foundCards = 0;
+
+let chosenCards = [];
+let chosenCardsIds = [];
+
+function initiateBoard() {
+    for (let i = 0; i < cardsList.length; i++) {
+        
+    }
+}
