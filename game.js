286fd0b2c3010d31021e48322ff1acd9b30dbf54
Timo <timokhan2@gmail.com>
2025-04-20 10:29:46 +0200

    cards randomised

--- game.js
+++ game.js
@@ -2,3 +2,5 @@ const cardsList = [
     'bird2.jpg', 'bird2.jpg', 'cat.jpg', 'cat.jpg', 'parrot.jpg', 'parrot.jpg',
     'ostrich.avif', 'ostrich.avif', 'squirrel.jpg', 'squirrel.jpg', 'turtle.jpg', 'turtle.jpg'  
 ];
+
+cardsList.sort(() => 0.5 - Math.random());
