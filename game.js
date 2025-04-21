d20708bc54582ab310411ebacb43b9e0b1076a84
Timo <timokhan2@gmail.com>
2025-04-21 23:12:15 +0200

    fetchCards function created

--- game.js
+++ game.js
@@ -1,8 +1,10 @@
-const cardsList = [
-    'bird2.jpg', 'bird2.jpg', 'cat.jpg', 'cat.jpg', 'parrot.jpg', 'parrot.jpg',
-    'ostrich.avif', 'ostrich.avif', 'squirrel.jpg', 'squirrel.jpg', 'turtle.jpg', 'turtle.jpg'  
-];
-cardsList.sort(() => 0.5 - Math.random());
+function fetchCards() {
+    return new Promise((resolve) => {
+        setTimeout (() => {
+            
+        })
+    })
+}
 
 const grid = document.querySelector(".gameGrid");
 const cardsInGame = 6;
